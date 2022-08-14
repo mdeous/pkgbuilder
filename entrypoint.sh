fa920aa589d22a653bceb1cd0bbc9a976c938d9a
Mathieu Deous <mat.deous@gmail.com>
2022-08-14 14:43:12 +0200

    more sensible args (I think?)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function build {
+    # install dependencies and build package
+    yay -S --noconfirm $(\
+        makepkg --printsrcinfo | \
+        grep -E '\b(make)?depends' | \
+        awk '{print $3}'\
+    )
+    makepkg --clean --cleanbuild --force --noconfirm $@
+}
+
 cmd="${1}"
 case "${cmd}" in
     makepkg)
@@ -7,13 +17,13 @@ case "${cmd}" in
         shift
         makepkg $@
         ;;
+    build)
+        # install deps and build
+        shift
+        build $@
+        ;;
     *)
-        # install dependencies and build package
-        yay -S --noconfirm $(\
-            makepkg --printsrcinfo | \
-            grep -E '\b(make)?depends' | \
-            awk '{print $3}'\
-        )
-        makepkg --clean --cleanbuild --force --noconfirm $@
+        # install deps and build
+        build $@
         ;;
 esac
