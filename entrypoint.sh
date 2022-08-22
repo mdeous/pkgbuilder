2448dcf730f628dc243d09932ca48e1f891796cf
Mathieu Deous <mat.deous@gmail.com>
2022-08-22 13:29:34 +0200

    update pacman cache before installing deps

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ function build {
     ); do
         sudo pacman-key --recv-keys "${key}"
     done
+    # update pacman cache
+    pacman -Sy
     # install dependencies
     yay -S --noconfirm $(\
         makepkg --printsrcinfo | \
